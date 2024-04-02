1551622d0af1a20bd830a19b66eab6947f58afb3
Marion <marion.1701@hotmail.fr>
2024-04-02 12:42:49 +0200

    first commit

---
+++ urls.js
@@ -0,0 +1,7 @@
+// Insert the backend URL given by Vercel in this variable
+const BACKEND_URL = 'https://my-movies-backend-zeta.vercel.app/';
+
+// Insert the frontend URL given by Vercel in this variable
+const FRONTEND_URL = 'https://my-movies-frontend-pi.vercel.app/';
+
+module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
