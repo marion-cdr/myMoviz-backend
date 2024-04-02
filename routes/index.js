c1bb169a1dc13c6d6368e1a995f97786b0f28280
Marion <marion.1701@hotmail.fr>
2024-04-02 14:51:21 +0200

    second commit

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch'); 
 
-const API_KEY = process.env.API_KEY
+// const API_KEY = process.env.API_KEY
 
 router.get('/movies', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=ee8d063f9e3483140eea927c147ab7fa')
     .then(res => res.json())
     .then (data => {
         res.json({ movies: data.results })
